9bc529553bb8382686106e08900a90a5e279046a
Liz Jones <lizbaconjones@gmail.com>
2021-10-17 11:45:03 -0500

    Intial commit with function to total hours coded.

---
+++ app.py
@@ -0,0 +1,13 @@
+
+hours_today = []
+def code_hours(x):
+    '''function to determine how many hours I have coded'''
+    hours_today = 'x'
+    tot_hours = 497.45
+    new_tots_hours = hours_today + tot_hours
+    
+    return print(f"You have coded for {new_tots_hours} hours!")
+
+    #print(hours)
+
+code_hours()
